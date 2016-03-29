04fadd23030d3f5f4477c39ef944d65f98f42d9d
Lordmau5 <dustin.hecken@web.de>
2016-03-29 20:42:29 +0200

    Hey look, I'm still pushing to this repo! :3

--- Firefox/resources/bttv4ffz/lib/main.js
+++ Firefox/resources/bttv4ffz/lib/main.js
@@ -2,7 +2,8 @@ var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
 	include: "*.twitch.tv",
-	contentScript: 'var s = document.createElement("script");' +
+	contentScript: 'var ce = document["createElement"].bind(document);' +
+					'var s = ce("script");' +
 					's.src = "//cdn.lordmau5.com/inject.js";' +
 					's.onload = function() {' +
 						'this.parentNode.removeChild(this);' +
--- inject.js
+++ inject.js
@@ -1,5 +1,5 @@
 // Version naming: (Main-version).(Sub-version)
-// Version: 1.0.7.4
+// Version: 1.0.7.5
 
 /*
     This file is being updated on my server (cdn.lordmau5.com) first before changes to the GitHub repo happen.
@@ -7,12 +7,21 @@
     This makes it easier for me to do updates when necessary (sudden FFZ-API changes, for example)
 */
 
+/*
+    TODO: First frame of GIFs when GIF-emotes are not enabled
+    Download image, draw (first frame) into canvas, get URI / base64 string, put in emotes
+    Somehow...
+*/
+
 (function(){
 
 // Global Storage / Settings
 
+var version = "1.0.7.5";
+
 var api,
     ffz,
+    current_channel,
     enable_global_emotes,
     enable_gif_emotes,
     enable_override_emotes;
@@ -22,9 +31,6 @@ var global_emotes_loaded = false,
 
 var override_emotes = [ ":'(", "D:" ];
 var isOverrideEmote = function(emote_regex) {
-    if (enable_override_emotes)
-      return false;
-
     for(var i = 0; i < override_emotes.length; i++) {
         if(emote_regex === override_emotes[i])
             return true;
@@ -38,7 +44,7 @@ var check_existance = function(attempts) {
     if (window.FrankerFaceZ !== undefined && window.jQuery !== undefined && window.App !== undefined) {
         // Register with FFZ.
         ffz = FrankerFaceZ.get();
-        api = ffz.api("BetterTTV", "https://cdn.betterttv.net/tags/developer.png");
+        api = ffz.api("BetterTTV", "https://cdn.betterttv.net/tags/developer.png", version);
 
         api.log("Injected successfully.");
 
@@ -66,15 +72,6 @@ var check_existance = function(attempts) {
 
 
 var doSettings = function() {
-
-    // TODO: Add version list-option
-    /*FrankerFaceZ.settings_info.bttv_version = {
-        category: "BTTV4FFZ",
-        name: "Current Version: 1.0.7.1"
-    };*/
-
-    //enable_override_emotes = ffz.settings.get("bttv_enable_override_emotes");
-
     FrankerFaceZ.settings_info.bttv_global_emotes = {
         type: "boolean",
         value: true,
@@ -91,27 +88,45 @@ var doSettings = function() {
             if (enabled) {
                 api.register_global_set(1);
 
-                if(enable_gif_emotes && gif_emotes_loaded)
+                if(enable_gif_emotes)
                     api.register_global_set(2);
+
+                if(enable_override_emotes)
+                    api.register_global_set(3);
             }
             else {
                 api.unregister_global_set(1);
-
-                if(enable_gif_emotes && gif_emotes_loaded)
-                    api.unregister_global_set(2);
+                api.unregister_global_set(2);
+                api.unregister_global_set(3);
             }
+
+            enable_global_emotes = enabled;
         }
     };
 
     enable_global_emotes = ffz.settings.get("bttv_global_emotes");
 
-
     FrankerFaceZ.settings_info.bttv_allow_gif_emotes = {
         type: "boolean",
         value: false,
         category: "BTTV4FFZ",
-        name: "GIF Emoticons <span>(Refresh required!)</span>",
-        help: "Enable this to show those shitty things."
+        name: "GIF Emoticons",
+        help: "Enable this to show GIF emotes.",
+        on_update: function(enabled) {
+            if (enabled) {
+                if(enable_global_emotes)
+                  api.register_global_set(2);
+
+                api.register_room_set(current_channel, 9002);
+            }
+            else {
+                api.unregister_global_set(2);
+
+                api.unregister_room_set(current_channel, 9002);
+            }
+
+            enable_gif_emotes = enabled;
+        }
     };
 
     enable_gif_emotes = ffz.settings.get("bttv_allow_gif_emotes");
@@ -120,8 +135,19 @@ var doSettings = function() {
         type: "boolean",
         value: true,
         category: "BTTV4FFZ",
-        name: "Enable Override Emotes <span>(Refresh required!)</span>",
-        help: "Enable this to show override emotes (like D:)."
+        name: "Enable Override Emotes",
+        help: "Enable this to show override emotes (like D:).",
+        on_update: function(enabled) {
+            if (enabled) {
+                if(enable_global_emotes)
+                  api.register_global_set(3);
+            }
+            else {
+                api.unregister_global_set(3);
+            }
+
+            enable_override_emotes = enabled;
+        }
     };
 
     enable_override_emotes = ffz.settings.get("bttv_enable_override_emotes");
@@ -136,7 +162,10 @@ var setupChannelLoading = function() {
 var channelCallback = function(room_id, reg_function, attempts) {
     $.getJSON("https://api.betterttv.net/2/channels/" + room_id)
         .done(function(data) {
+            current_channel = room_id;
+
             var channelBTTV = new Array(),
+                channelBTTV_GIF = new Array(),
                 emotes = data["emotes"];
 
             for(var i = 0; i < emotes.length; i++) {
@@ -147,9 +176,9 @@ var channelCallback = function(room_id, reg_function, attempts) {
 
                     xMote = {
                         urls: {
-                            1: "//cdn.betterttv.net/emote/" + id + "/1x",
-                            2: "//cdn.betterttv.net/emote/" + id + "/2x",
-                            4: "//cdn.betterttv.net/emote/" + id + "/3x"
+                            1: "https://cdn.betterttv.net/emote/" + id + "/1x",
+                            2: "https://cdn.betterttv.net/emote/" + id + "/2x",
+                            4: "https://cdn.betterttv.net/emote/" + id + "/3x"
                         },
                         id: id,
                         name: emote["code"],
@@ -162,18 +191,30 @@ var channelCallback = function(room_id, reg_function, attempts) {
                         require_spaces: req_spaces
                     };
 
-                if (emote["imageType"] === "png" || (emote["imageType"] === "gif" && enable_gif_emotes))
+
+                if (emote["imageType"] === "png")
                     channelBTTV.push(xMote);
+
+                if (emote["imageType"] === "gif")
+                    channelBTTV_GIF.push(xMote);
             }
 
             if (!channelBTTV.length)
                 return;
 
             var set = {
-                emoticons: channelBTTV
+                emoticons: channelBTTV,
+                title: "BetterTTV Emoticons"
             };
 
-            reg_function(room_id, set);
+            api.register_room_set(room_id, 9001, set); // Load normal emotes
+
+            set = {
+                emoticons: channelBTTV_GIF,
+                title: "BetterTTV Emoticons (GIF)"
+            };
+
+            api.register_room_set(room_id, 9002, set); // Load GIF emotes
 
         }).fail(function(data) {
             if (data["status"] === 404) {
@@ -194,6 +235,7 @@ var implementBTTVGlobals = function(attempts) {
         .done(function(data) {
             var globalBTTV = new Array(),
                 globalBTTV_GIF = new Array(),
+                overrideEmotes = new Array(),
 
                 emotes = data["emotes"];
 
@@ -204,7 +246,7 @@ var implementBTTVGlobals = function(attempts) {
                     match = /cdn.betterttv.net\/emote\/(\w+)/.exec(emote["url"]),
                     id = match && match[1];
 
-                if (emote["channel"] || isOverrideEmote(emote["regex"]))
+                if (emote["channel"])
                     continue;
 
                 var xMote = {
@@ -218,9 +260,9 @@ var implementBTTVGlobals = function(attempts) {
                 if (id) {
                     xMote["id"] = id;
                     xMote["urls"] = {
-                        1: "//cdn.betterttv.net/emote/" + id + "/1x",
-                        2: "//cdn.betterttv.net/emote/" + id + "/2x",
-                        4: "//cdn.betterttv.net/emote/" + id + "/3x"
+                        1: "https://cdn.betterttv.net/emote/" + id + "/1x",
+                        2: "https://cdn.betterttv.net/emote/" + id + "/2x",
+                        4: "https://cdn.betterttv.net/emote/" + id + "/3x"
                     };
                 }
 
@@ -228,23 +270,29 @@ var implementBTTVGlobals = function(attempts) {
                 if(emote["regex"] === "SoSnowy" || emote["regex"] === "CandyCane" || emote["regex"] === "ReinDeer" || emote["regex"] === "SantaHat")
                      xMote["margins"] = "-8px 8px 0px -30px";
 
-                emote["imageType"] === "gif" ? globalBTTV_GIF.push(xMote) : globalBTTV.push(xMote);
+                if(isOverrideEmote(emote["regex"]))
+                    overrideEmotes.push(xMote);
+                else {
+                  emote["imageType"] === "gif" ? globalBTTV_GIF.push(xMote) : globalBTTV.push(xMote);
+                }
             }
 
             var set = {
                 emoticons: globalBTTV
             };
-
             api.register_global_set(1, set);
 
-            if (enable_gif_emotes) {
-                set = {
-                    emoticons: globalBTTV_GIF,
-                    title: "Global Emoticons (GIF)"
-                };
-                api.register_global_set(2, set);
-                gif_emotes_loaded = true;
-            }
+            set = {
+                emoticons: globalBTTV_GIF,
+                title: "Global Emoticons (GIF)"
+            };
+            api.register_global_set(2, set);
+
+            set = {
+                emoticons: overrideEmotes,
+                title: "Global Emoticons (Override)"
+            };
+            api.register_global_set(3, set);
 
             global_emotes_loaded = true;
 
