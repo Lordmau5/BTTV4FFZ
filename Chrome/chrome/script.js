c15f1d6e658c80ad2ff4b83406c5d873e49f70ca
Lordmau5 <dustin.hecken@web.de>
2016-10-22 14:44:19 +0200

    Revert to actual CDN URL

--- Chrome/chrome/script.js
+++ Chrome/chrome/script.js
@@ -1,6 +1,6 @@
 function inject() {
     var s = document.createElement('script');
-    s.src = "https://lordmau5.com/nocache/inject.js"; //"//cdn.lordmau5.com/inject.js";
+    s.src = "https://cdn.lordmau5.com/inject.js";
     s.onload = function() {
         this.parentNode.removeChild(this);
     };
